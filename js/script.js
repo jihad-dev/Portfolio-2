1c4a0c457b877282b0f24f11126d11c582dee378
jihad <mdjihadislam166@gmail.com>
2023-02-09 20:08:11 +0600

    loo

--- js/script.js
+++ js/script.js
@@ -5,4 +5,67 @@ var typed = new Typed(".typing", {
     loop: true
 });
 
-// //
+// ////////////////////
+
+
+(function(){
+
+    'use strict';
+
+
+    var $projects = $('.projects');
+
+    $projects.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+
+    $('ul.filters > li').on('click', function(e){
+
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+
+        $projects.isotope({filter: filter});
+
+    });
+
+    $('.card').mouseenter(function(){
+
+        $(this).find('.card-overlay').css({'top': '-100%'});
+        $(this).find('.card-hover').css({'top':'0'});
+
+    }).mouseleave(function(){
+
+        $(this).find('.card-overlay').css({'top': '0'});
+        $(this).find('.card-hover').css({'top':'100%'});
+
+    });
+
+})(jQuery);
+////
+// team section //
+var swiper = new Swiper(".team-slider", {
+    loop:true,
+    // grabCursor:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+    },
+  });
+  
